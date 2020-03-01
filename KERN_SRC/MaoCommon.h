079778e85b2cdd6af15502fe0eb953a0003bb3c3
Jianwei Mao <maojianwei2012@126.com>
2020-03-01 17:14:08 +0800

    add ntohs

--- KERN_SRC/MaoCommon.h
+++ KERN_SRC/MaoCommon.h
@@ -1,5 +1,5 @@
 
-unsigned short mao_ntohs_htons(char * firstP)
+inline unsigned short mao_ntohs_htons(char * firstP)
 {
 	unsigned short ret;
 	*((char*)(&ret)) = firstP[1];
@@ -7,14 +7,20 @@ unsigned short mao_ntohs_htons(char * firstP)
 	return ret;
 }
 
-unsigned int mao_ntohl_htonl(char * firstP)
+inline unsigned int mao_ntohl_htonl(char * firstP)
 {
+	//no use
 	unsigned int ret;
 	*((char*)(&ret)) = firstP[3];
 	*(((char*)(&ret))+1) = firstP[2];
 	*(((char*)(&ret))+2) = firstP[1];
 	*(((char*)(&ret))+3) = firstP[0];
 	return ret;
+}
+
+inline unsigned short mao_ntohs_htons_val(unsigned short val)
+{
+	return mao_ntohs_htons((char*)(&val));
 }
 
 #define m2s(firstP) mao_ntohs_htons(firstP)
